5503e830d2085f5a58486c9947f9ede05517c488
BossPC <sutthikiat.ph@gmail.com>
2020-10-10 23:27:01 +0700

    add document

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ WORKDIR /usr/src/app
 COPY package.json /usr/src/app/
 RUN yarn install
 COPY . /usr/src/app
+ENV PORT=3000 NODE_ENV='production' STATUS_PROJECT='production'
 RUN yarn build 
-CMD [ "npm" , 'run', "start" ]
+CMD [ "npm", "start" ]
--- server.js
+++ server.js
@@ -6,19 +6,19 @@ const dev = false;
 const app = next({ dev });
 const handle = app.getRequestHandler();
 const port = process.env.PORT || 3000;
-// app
-//   .prepare()
-//   .then(() => {
-//     const server = express();
-//     server.get("*", (req, res) => {
-//       return handle(req, res);
-//     });
-//     server.listen(port, (err) => {
-//       if (err) throw err;
-//       console.log(`> Ready on http://localhost:${process.env.PORT || 3000} !!`);
-//     });
-//   })
-//   .catch((ex) => {
-//     console.error(ex.stack);
-//     process.exit(1);
-//   });
+app
+  .prepare()
+  .then(() => {
+    const server = express();
+    server.get("*", (req, res) => {
+      return handle(req, res);
+    });
+    server.listen(port, (err) => {
+      if (err) throw err;
+      console.log(`> Ready on http://localhost:${process.env.PORT || 3000} !!`);
+    });
+  })
+  .catch((ex) => {
+    console.error(ex.stack);
+    process.exit(1);
+  });
